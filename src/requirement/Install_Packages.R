9d71c5211b9fef6649d0c9423e8ebfd34ad1c79d
Pedro <pedrocastro014@gmail.com>
2025-07-25 08:50:57 -0300

    Added TODO

--- src/requirement/Install_Packages.R
+++ src/requirement/Install_Packages.R
@@ -1,1 +1,16 @@
-# Check if the user has the necessary packages installed
+#############
+# Check and install the necessary package ----
+#############
+
+Install_Packages <- function(pkgs) {
+  
+  # Get which packages are not installed
+  notInstalled <- pkgs[!pkgs %in% installed.packages()[, "Package"]]
+  
+  # Install CRAN packages
+  if (length(notInstalled) > 0) {
+    install.packages(notInstalled)
+  }
+  
+  #TODO: What if they are avaiable only in github ou biocmanager?
+}
