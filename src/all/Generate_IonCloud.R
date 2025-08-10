e72233b595843c60b4bc328cdc04565fcaa160c8
Pedro <pedrocastro014@gmail.com>
2025-08-10 17:58:09 -0300

    precursorRT and ionCloud

---
+++ src/all/Generate_IonCloud.R
@@ -0,0 +1,39 @@
+#############
+# Generate the Íon cloud  ----
+
+# For the m/z map, the density of ions collected is plotted over
+# the scan range (m/z) for each sample.
+#############
+
+Generate_IonCloud <- function(dr, cols = 3, rows = NULL ) {
+  dr <- Generate_DiannReport(samples)
+   cols = 3
+  
+  ionCloud <- ggplot2::ggplot(dr, ggplot2::aes(x = RT, y = Precursor.Mz)) +
+    ggpointdensity::geom_pointdensity(size = 0.1) +
+    viridis::scale_color_viridis(option = "H") +
+    ggplot2::scale_x_continuous(limits = c(0, 90)) +
+    ggplot2::labs(
+      x = "Retention time (min)",
+      y = "Scan range (m/z)",
+      color = NULL
+    ) +
+    ggplot2::facet_wrap(~Run,
+                        labeller = ggplot2::as_labeller(labels_names),
+                        scales = "free",
+                        ncol = cols) +
+    ggplot2::theme(
+      strip.background = ggplot2::element_blank(),
+      legend.position = "bottom",
+      legend.key.width = ggplot2::unit(1.5, "cm"),
+      legend.key.height = ggplot2::unit(0.25, "cm"),
+      panel.border = ggplot2::element_rect(color = "black", fill = NA),
+      panel.background = ggplot2::element_blank()
+    )
+  
+  return(ionCloud)
+}
+  
+  
+  
+  
