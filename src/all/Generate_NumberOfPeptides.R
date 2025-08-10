923b691d8a0819925acf4a39edfc66b32ba09f1d
Pedro <pedrocastro014@gmail.com>
2025-08-10 18:06:22 -0300

    forgot to remove some values inside the function

---
+++ src/all/Generate_NumberOfPeptides.R
@@ -0,0 +1,10 @@
+#############
+# Generate Number of Peptides  ----
+
+# Counting the number of unique peptides per run
+#############
+
+
+Generate_NumberOfPeptides <- function(dr, colors) {
+  dr <- Generate_DiannReport(samples)
+}
