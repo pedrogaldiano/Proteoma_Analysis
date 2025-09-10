c0c67cd48c577b0f2b99b7df3f385cf27d8f173e
Pedro <pedrocastro014@gmail.com>
2025-09-10 14:29:52 -0300

    limma compare results done!

--- src/all/Create_LimmaMatrix.R
+++ src/all/Create_LimmaMatrix.R
@@ -4,11 +4,8 @@
 #This file contain all limma related functions
 #############
 
-Create_ContrastFitQuantUMS <- function(imtx) {
-  imtx <- imputedMatrix
-  comparison <- samplesToCompare
-  method = "robust" #or "lm"
-  
+Create_ContrastFitQuantUMS <- function(imtx, comparisonList, method = "robust" ) {
+
   if (!method %in% c("robust", "lm"))
   {
     cat("method should recieve 'lm' or 'robust'.")
@@ -28,7 +25,7 @@ Create_ContrastFitQuantUMS <- function(imtx) {
   
   contrastMatrix <- do.call(
     limma::makeContrasts,
-    c(comparison, list(levels = designGroups))
+    c(comparisonList, list(levels = designGroups))
   )  
   
   fitQuantUMS <- limma::lmFit(mtx, designGroups, method = method)
@@ -38,4 +35,47 @@ Create_ContrastFitQuantUMS <- function(imtx) {
     )
   
   return(contrastFitQuantUMS)
+}
+
+
+ApplyLimmaTopTable <- function(contrastFitQuantUMS, comparison) {
+  
+  limmaResult <- limma::topTable(contrastFitQuantUMS,
+                                 coef = comparison,
+                                 number = Inf,
+                                 adjust.method = "BH") |> 
+    tibble::rownames_to_column("proteins") |>
+    dplyr::mutate(
+      status = dplyr::case_when(
+        logFC > 0.58 & adj.P.Val <= 0.05 ~ "Increased",
+        logFC < -0.58 & adj.P.Val <= 0.05 ~ "Decreased",
+        TRUE ~ "Not significant"),
+      status = factor(status, 
+                      levels = c("Decreased", "Not significant", "Increased")),
+      condition = comparison
+    )
+  
+  return(limmaResult)
+}
+
+CompareLimmaResults <- function(contrastFit,
+                                comparisonList) {
+  
+  resultQuantUMS <- list() # Result from samples comparison
+
+  for (item in names(comparisonList)) {
+    limmaResult <- ApplyLimmaTopTable(contrastFit, item)
+    resultQuantUMS <- dplyr::bind_rows(resultQuantUMS,limmaResult)
+  }
+  
+  result <- resultQuantUMS |> 
+    dplyr::mutate(logFC = round(logFC, 2),
+                  AveExpr = round(AveExpr, 2),
+                  B = round(B, 2)) |>
+    dplyr::rename(Protein = proteins,
+                  Comparison = condition)
+  
+  cat("\n\nComparisons between groups done.\n")
+  
+  return(result)
 }
