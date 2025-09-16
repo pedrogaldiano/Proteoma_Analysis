6cdb8f1de4895c075d21ce1cc99135f2855695dd
Pedro <pedrocastro014@gmail.com>
2025-09-16 20:21:32 -0300

    MICE for data imputation

---
+++ src/all/Create_ImputationMatrix_MICE.R
@@ -0,0 +1,56 @@
+#############
+# Create Imputation Matrix MICE  ----
+
+# Impute values to NA places using MICE package
+
+#############
+
+
+Create_ImputarionMatrix_MICE <- function(dm,
+                                         methodValue,
+                                         override = TRUE,
+                                         mValue = 5,
+                                         maxitValue = 5,
+                                         seed = 123,
+                                         path = "././data/temp_files/") {
+  
+  
+  if (!methodValue %in% c("rf", "pmm")) {
+    cat("\nMethodValue is not valid.\nPlease select 'rf' or 'pmm'.")
+  }
+  
+  fileName <- paste0(path, "result_imputation_MICE.rds")
+  fileExist <- file.exists(fileName)
+  
+  if (!(override) & fileExist) {
+    
+    cat("\nLoad file:", fileName,"\n")
+    return(readRDS(fileName))
+  } else {
+    
+    
+    matrixForImputation <- log2(dm + 1) |>
+      as.data.frame() |>
+      tibble::rownames_to_column(var = "protein")
+  
+    cat("\nCreating the imputed matrix.\n\nThis is going to take a while.\n")
+    
+    
+    # methods = rf (random forest imputations), pmm (predictive mean matching)
+    imputedMatrix <- mice::mice(matrixForImputation,
+                                method = methodValue,
+                                m = mValue,
+                                maxit = maxitValue,
+                                seed = seed)
+    
+    result <- mice::complete(imputedMatrix)
+    
+    saveRDS(result, file = fileName)
+    
+    return(result)
+  }
+}
+
+
+
+
