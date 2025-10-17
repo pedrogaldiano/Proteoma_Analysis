f23c53b4e97dd8fbdb1e9bc27de479c16b6f0005
Pedro <pedrocastro014@gmail.com>
2025-10-16 22:11:17 -0300

    I think it should be able to accept REP_9999 or REP_ZZZZ

--- src/all/Create_ImputationMatrix_StepWise.R
+++ src/all/Create_ImputationMatrix_StepWise.R
@@ -20,7 +20,7 @@
 Create_ImputationMatrix_StepWise <- function(dm) {
   
   matrixForImputation <- log2(dm + 1)
-  groupLabels <- stringr::str_remove(colnames(matrixForImputation), "_REP_\\d+")
+  groupLabels <- stringr::str_remove(colnames(matrixForImputation), "_REP_....$") #"_REP_\\d+"
   
   imputedData <- StepWiseImputation(
     protein_matrix = matrixForImputation,
