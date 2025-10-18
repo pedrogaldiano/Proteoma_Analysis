03bf13ddbdaa615ab65fcfd9c4814969d6308b91
Pedro <pedrocastro014@gmail.com>
2025-10-18 10:13:13 -0300

    create a LoadSamples.R to hold the commun aspects between Basic_Analysis and Imputation

--- src/all/Generate_DiannReport.R
+++ src/all/Generate_DiannReport.R
@@ -1,30 +1,34 @@
 #############
 # Generate diann report  ----
 
-# Generate the diannReport.rds from report.parquet file using arrow library 
+# Generate the diannReport.rds from report.parquet file using arrow library
 # and making some basic filtering
 #############
 
 Generate_DiannReport <- function(
-    mapping,
-    organism = "",
-    parquetDIR = "./data/input/report.parquet"
+  mapping,
+  organism = "",
+  parquetDIR = "./data/input/report.parquet"
 ) {
   report <- arrow::read_parquet(parquetDIR)
-  
+
   report <- dplyr::filter(
     report,
     Lib.PG.Q.Value <= 0.01 &
       Lib.Q.Value <= 0.01 &
       PG.Q.Value <= 0.01
   )
-        
+
   if (organism != "") {
-    report <- dplyr::filter(report, stringr::str_detect(Protein.Names, organism))
+    report <- dplyr::filter(
+      report,
+      stringr::str_detect(Protein.Names, organism)
+    )
   }
-  
-  report <- dplyr::left_join(report, mapping, by = c("Run" = "Run")) 
-  
+
+  #TODO: I don't need all columns to mix to report, maybe a inner join is better for this scenario!
+  report <- dplyr::left_join(report, mapping, by = c("Run" = "Run"))
+
   report <- dplyr::mutate(
     report,
     Run = report$SampleName,
@@ -33,6 +37,6 @@ Generate_DiannReport <- function(
     File.Name = Run,
     peptide_length = nchar(Stripped.Sequence)
   )
-  
+
   return(report)
 }
